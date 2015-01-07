e074f81af400da6b41512bb20d5ee5dce96c5f5f
jopk <biasejok@gmail.com>
2015-01-07 16:22:26 +0100

    analiza qrstr z bezpiecznym haslem

--- analizeQRString.js
+++ analizeQRString.js
@@ -1,18 +1,24 @@
 function analyzeStringFromQR(str) {
-    var w = new Array(2);
-    var tmp = "";
-    var n = str.length - 1;
-    for (var i = str.length-64; i < str.length; i++) {
-        tmp += str[i];
+    var ADR = 30,
+        SHA = 64,
+        SR = 16;
+    var w = new Array(3);
+    var path = "";
+    for (var i = 0; i <= ADR + SHA; i++) {
+        path += str[i];
     }
-    w[0] = tmp;
+    w[0] = path;
 
-    tmp = "";
-    var p = i;
-    for (i = 0; i < str.length-64; i++) {
-        tmp += str[i];
+    var pass = "";
+    for (var i = ADR+SHA+1; i <= ADR+SHA+SR; i++) {
+        pass += str[i];
     }
-    w[1] = tmp;
+    w[1] = pass;
+    var name = "";
+    for (var i = ADR+SHA+SR+1; i < str.length; i++) {
+        name += str[i];
+    }
+    w[2] = name;
 
     return w;
 }
